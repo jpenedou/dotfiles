ddea74b34659c1b9a57cbc79f37285d59f6dc06d
Jose Antonio <japenedo@gmail.com>
2024-08-26 22:11:29 +0200

    Se eliminan comentarios innecesarios

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#  ____                               _           _
-# / ___|  ___ _ __ ___  ___ _ __  ___| |__   ___ | |_
-# \___ \ / __| '__/ _ \/ _ \ '_ \/ __| '_ \ / _ \| __|
-#  ___) | (__| | |  __/  __/ | | \__ \ | | | (_) | |_
-# |____/ \___|_|  \___|\___|_| |_|___/_| |_|\___/ \__|
-#
-#
-# by Stephan Raabe (2023)
-# -----------------------------------------------------
 
 DIR="$HOME/Pictures/screenshots/"
 NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
@@ -21,12 +12,12 @@ choice=$(echo -e "$options" | rofi -dmenu -replace -i -no-show-icons -l 2 -width
 
 case $choice in
 $option2)
-	grim -g "$(slurp)" - | swappy -f -
-	notify-send "Screenshot created" "Mode: Selected area"
-	;;
+  grim -g "$(slurp)" - | swappy -f -
+  notify-send "Screenshot created" "Mode: Selected area"
+  ;;
 $option3)
-	sleep 3
-	grim - | swappy -f -
-	notify-send "Screenshot created" "Mode: Fullscreen"
-	;;
+  sleep 3
+  grim - | swappy -f -
+  notify-send "Screenshot created" "Mode: Fullscreen"
+  ;;
 esac
