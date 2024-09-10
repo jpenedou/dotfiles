bf547740a1ac992fea62a2e0c524d51dcab25ac7
Jose Antonio <japenedo@gmail.com>
2024-09-10 17:27:10 +0200

    Se eliminan windowrules no necesarias. Monitors config preestablecida en fichero, se elimina cuando el chasis es laptop

--- .config/hypr/scripts/machine.sh
+++ .config/hypr/scripts/machine.sh
@@ -8,14 +8,10 @@ if [[ $chassis = "laptop" ]]; then
   # kb_options
   hyprctl keyword input:kb_options caps:escape_shifted_capslock,altwin:prtsc_rwin
   hyprctl keyword bind CTRL, SUPER_R, exec, ~/.config/hypr/scripts/screenshot.sh
-
+  hyprctl keyword monitor HDMI-A-1, disable
+  hyprctl keyword monitor HDMI-A-2, disable
+  hyprctl dispatch workspace 1
 else
   # binds
   hyprctl keyword bind ", PRINT, exec, ~/.config/hypr/scripts/screenshot.sh"
-
-  # monitors
-  hyprctl keyword monitor HDMI-A-1,1920x1080,1080x480,1
-  hyprctl keyword monitor HDMI-A-2,1920x1080,0x0,1,transform,1
-  hyprctl keyword workspace 1, monitor:HDMI-A-2
-  hyprctl keyword workspace 2, monitor:HDMI-A-1
 fi
