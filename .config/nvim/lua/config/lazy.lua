bd0a1a1a7d443d003a1f6c561cf8ebc2db2d5204
Jose Antonio <japenedo@gmail.com>
2024-09-25 22:37:26 +0200

    lazyvim, toggleterm keybinding changed.
    Añadido extras.test.core

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
+    { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
       require("toggleterm").setup({
-        open_mapping = [[<leader>t]],
+        open_mapping = [[<leader>T]],
         shade_terminals = false,
         insert_mappings = false,
         -- shell = "pwsh.exe",
@@ -12,7 +12,7 @@ return {
     version = "*",
     keys = {
       {
-        "<leader>t",
+        "<leader>T",
         "<cmd>ToggleTerm size=20 direction=horizontal<cr>",
         desc = "Open a horizontal terminal below",
         mode = { "n" },
