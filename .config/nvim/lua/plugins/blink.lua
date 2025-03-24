5176caf857730dd400235ac1027377e25e8c4033
Jose Antonio <japenedo@gmail.com>
2025-03-24 10:40:29 +0100

    nvim, emoji support

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,30 @@
 return {
   "saghen/blink.cmp",
+  dependencies = { "allaman/emoji.nvim", "saghen/blink.compat" },
   opts = {
     keymap = {
       preset = "super-tab",
       ["<CR>"] = { "accept", "fallback" },
+    },
+    sources = {
+      default = {
+        ...,
+        "emoji",
+      },
+      providers = {
+        emoji = {
+          name = "emoji",
+          module = "blink.compat.source",
+          -- overwrite kind of suggestion
+          transform_items = function(ctx, items)
+            local kind = require("blink.cmp.types").CompletionItemKind.Text
+            for i = 1, #items do
+              items[i].kind = kind
+            end
+            return items
+          end,
+        },
+      },
     },
   },
 }
---
+++ .config/nvim/lua/plugins/emoji.lua
@@ -0,0 +1,7 @@
+return {
+  "allaman/emoji.nvim",
+  opts = {
+    -- default is false, also needed for blink.cmp integration!
+    enable_cmp_integration = true,
+  },
+}
