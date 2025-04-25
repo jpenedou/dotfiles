7ae7a8de2810768b0b679cab5aa6cf02295cc50b
Jose Antonio <japenedo@gmail.com>
2025-04-25 20:37:19 +0200

    nvim, yazi override keybinding deleted

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -16,11 +16,11 @@ return {
       "<cmd>Yazi cwd<cr>",
       desc = "Abrir Yazi en el directorio raíz del proyecto",
     },
-    {
-      "<C-Up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Volver a la última sesión de Yazi",
-    },
+    -- {
+    --   "<C-Up>",
+    --   "<cmd>Yazi toggle<cr>",
+    --   desc = "Volver a la última sesión de Yazi",
+    -- },
   },
   opts = {
     open_for_directories = true, -- Si quieres abrir carpetas directamente en Yazi
