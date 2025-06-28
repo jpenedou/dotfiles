dace9837314d96097746fb0981e9f7ff0a48dc74
Jose Antonio <japenedo@gmail.com>
2025-06-29 00:18:09 +0200

    fix(blink): fixed error nil value

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -4,6 +4,17 @@ return {
   opts = {
     keymap = {
       preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
       ["<CR>"] = { "accept", "fallback" },
     },
     sources = {
