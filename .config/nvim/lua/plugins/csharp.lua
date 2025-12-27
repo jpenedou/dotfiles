9e3fdfd297aa7e457f9304281133df6efbbdc082
Jose Antonio <japenedo@gmail.com>
2025-12-27 17:28:37 +0100

    nvim, disable omnisharp

--- .config/nvim/lua/plugins/csharp.lua
+++ .config/nvim/lua/plugins/csharp.lua
@@ -3,14 +3,20 @@ return {
   {
     "mason-org/mason.nvim",
     opts = {
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-      },
-      ensure_installed = {
-        "roslyn",
-      },
+      registries = { "github:mason-org/mason-registry", "github:Crashdummyy/mason-registry" },
+      ensure_installed = { "roslyn" },
     },
+  },
+  -- Evitar que Omnisharp sea configurado por lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.setup = opts.setup or {}
+      opts.setup["omnisharp"] = function()
+        return true
+      end
+    end,
   },
   -- Configurar Roslyn LSP
   {
