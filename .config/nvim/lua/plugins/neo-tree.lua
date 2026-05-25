ed5007bf62e6ee68b6e7bbdfb21caf79d9287849
Jose Antonio <japenedo@gmail.com>
2026-05-25 16:33:17 +0200

    nvim, use snacks_explorer instead of neo-tree and removed toggleterm

--- .config/nvim/lua/plugins/neo-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      position = "right",
-    },
-    -- sources = { "filesystem", "buffers", "git_status", "netman.ui.neo-tree" },
-  },
-}
